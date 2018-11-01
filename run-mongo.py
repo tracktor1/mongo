0b78ff3d9802909d6635be8f67629d60ceb5df34
tracktor1 <39525540+tracktor1@users.noreply.github.com>
2018-11-01 16:33:57 +0200

    Update run-mongo.py

--- run-mongo.py
+++ run-mongo.py
@@ -2,10 +2,22 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("user", action="store_true")
+parser.add_argument("default", default="no_arg")
+parser.add_argument("user", help='create user only', default="no_arg")
+
+#parser.add_argument("user", action="store_true")
 args = parser.parse_args()
-#if args.user == "True"
+#if args.user == True:
+#	print("Deep Shit")
+#elif args.user == user:
+#	print(args.user)
 #    args.user = "run all"
 
-if args == user:
-	print(args.user)
+#parser.add_argument("user", nargs='?', default="no_arg")
+
+if args.user == "no_arg":
+    print ("I can tell that no argument was given and I can deal with that here.")
+elif args.user == 'magic.name':
+    print ("You nailed it!")
+else:
+    print (args.user)
