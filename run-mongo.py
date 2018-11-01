031c97da24a7f6352930a00cdb700eecd978423f
tracktor1 <39525540+tracktor1@users.noreply.github.com>
2018-11-01 13:49:29 +0200

    Update run-mongo.py

--- run-mongo.py
+++ run-mongo.py
@@ -4,6 +4,6 @@ import argparse
 parser = argparse.ArgumentParser(argument_default=shit)
 parser.add_argument("user", action="store_true")
 args = parser.parse_args()
-if args.user == "True"
-    args.user = "run all"
+#if args.user == "True"
+#    args.user = "run all"
 print(args.user)
