5fbf4307f830730dadfce3d68eb03246e78ba3c0
David <39525540+tracktor1@users.noreply.github.com>
2018-11-03 20:04:32 +0200

    Update run.py

--- run.py
+++ run.py
@@ -18,13 +18,5 @@ if not os.path.exists(playbook_path):
     print ('[INFO] The playbook does not exist in path', playbook_path)
     sys.exit()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-u", "--user", help='create user only', default="no_arg")
-args = parser.parse_args()
-
-if args.user == "no_arg":
-    print ("I can tell that no argument was given and I can deal with that here.")
-elif args.user == 'user':
-    print ("You nailed it!")
-else:
-    print ("Wrong Parameter, only option is -u user")
+	
+subprocess.call("ansible-playbook -K ", playbook_path)
