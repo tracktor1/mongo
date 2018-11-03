22aff95f1b28ede4b71d45a3fae58a1a77e2736a
David <39525540+tracktor1@users.noreply.github.com>
2018-11-03 20:27:12 +0200

    Update run-mongo.py

--- run-mongo.py
+++ run-mongo.py
@@ -12,9 +12,9 @@ args = parser.parse_args()
 
 if args.user == "no_arg":
     playbook_path = './mongo.yml'
-    if not os.path.exists(playbook_path):
-        print ('[INFO] The playbook does not exist in path', playbook_path)
-        sys.exit()
+    #if not os.path.exists(playbook_path):
+    #    print ('[INFO] The playbook does not exist in path', playbook_path)
+    #    sys.exit()
 
 	subprocess.call(["ansible-playbook", "-K", "playbook_path"])
 elif args.user == 'user':
