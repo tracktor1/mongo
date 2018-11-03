3ea0c41484d54bfb5de883eac456701a515e2eb4
David <39525540+tracktor1@users.noreply.github.com>
2018-11-03 20:17:05 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,15 +4,6 @@ import sys
 import subprocess
 import argparse
 
-#import ansible
-#from collections import namedtuple
-#from ansible.parsing.dataloader import DataLoader
-#from ansible.vars import VariableManager
-#from ansible.inventory import Inventory
-#from ansible.playbook.play import Play
-#from ansible.executor.playbook_executor import PlaybookExecutor
-
-
 playbook_path = './mongo.yml'
 if not os.path.exists(playbook_path):
     print ('[INFO] The playbook does not exist in path', playbook_path)
