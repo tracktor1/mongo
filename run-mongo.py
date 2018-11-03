110d90ad8507a701f3ec0b1704cc0147d4d3ac69
David <39525540+tracktor1@users.noreply.github.com>
2018-11-03 20:15:33 +0200

    Update run-mongo.py

--- run-mongo.py
+++ run-mongo.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 import os
+import sys
+import subprocess
 import argparse
-from ansible.parsing.dataloader import DataLoader
-from ansible.vars import VariableManager
-from ansible.inventory import Inventory
-from ansible.playbook.play import Play
-from ansible.executor.playbook_executor import PlaybookExecutor
 
 
 parser = argparse.ArgumentParser()
-
 parser.add_argument("-u", "--user", help='create user only', default="no_arg")
 args = parser.parse_args()
 
+playbook_path = './mongo.yml'
+
 if args.user == "no_arg":
-    print ("I can tell that no argument was given and I can deal with that here.")
+    if not os.path.exists(playbook_path):
+    print ('[INFO] The playbook does not exist in path', playbook_path)
+    sys.exit()
+	subprocess.call(["ansible-playbook", "-K", "./mongo.yml"])
 elif args.user == 'user':
     print ("You nailed it!")
 else:
