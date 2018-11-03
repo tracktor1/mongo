13fc82976bd0a7544ad0cd38c9db829428fcf24b
David <39525540+tracktor1@users.noreply.github.com>
2018-11-03 19:30:56 +0200

    Update run.py

--- run.py
+++ run.py
@@ -14,8 +14,17 @@ import argparse
 
 
 playbook_path = './mongo.yml'
-
 if not os.path.exists(playbook_path):
     print ('[INFO] The playbook does not exist in path', playbook_path)
     sys.exit()
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--user", help='create user only', default="no_arg")
+args = parser.parse_args()
+
+if args.user == "no_arg":
+    print ("I can tell that no argument was given and I can deal with that here.")
+elif args.user == 'user':
+    print ("You nailed it!")
+else:
+    print ("Wrong Parameter, only option is -u user")
