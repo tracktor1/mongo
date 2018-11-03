996f78ec1fb141ddb668b4394777d9e74a60362b
David <39525540+tracktor1@users.noreply.github.com>
2018-11-03 23:55:26 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,11 +4,13 @@ import sys
 import subprocess
 import argparse
 
-playbook_path = './mongo.yml'
-if not os.path.exists(playbook_path):
-    print ('[INFO] The playbook does not exist in path', playbook_path)
-    sys.exit()
+#playbook_path = './mongo.yml'
+#if not os.path.exists(playbook_path):
+#    print ('[INFO] The playbook does not exist in path', playbook_path)
+#    sys.exit()
 
-	
-subprocess.call(["ansible-playbook", "-K", "./mongo.yml"])
+print(os.getcwd())
+print(os.path.dirname(os.path.abspath(__file__)))
+
+#subprocess.call(["ansible-playbook", "-K", "./mongo.yml"])
 #subprocess.call(["ping", "-c 2", "8.8.8.8"])
